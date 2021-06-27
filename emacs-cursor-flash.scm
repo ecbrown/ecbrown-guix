d2bfe4e4db153a615559cca399b2cdf9779032e0
Eric Brown <ecbrown@ericcbrown.com>
2021-06-26 20:38:34 -0500

    Add new packages

---
+++ emacs-cursor-flash.scm
@@ -0,0 +1,145 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2021 Eric Brown <ecbrown@ericcbrown.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (emacs-cursor-flash)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix cvs-download)
+  #:use-module (guix download)
+  #:use-module (guix bzr-download)
+  #:use-module (guix git-download)
+  #:use-module (guix hg-download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system cmake)
+  #:use-module (guix build-system copy)
+  #:use-module (guix build-system emacs)
+  #:use-module (guix build-system glib-or-gtk)
+  #:use-module (guix build-system perl)
+  #:use-module (guix build-system trivial)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages aspell)
+  #:use-module (gnu packages audio)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages cmake)
+  #:use-module (gnu packages code)
+  #:use-module (gnu packages cpp)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages databases)
+  #:use-module (gnu packages dictionaries)
+  #:use-module (gnu packages djvu)
+  #:use-module (gnu packages ebook)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages golang)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages haskell-apps)
+  #:use-module (gnu packages ibus)
+  #:use-module (gnu packages java)
+  #:use-module (gnu packages ncurses)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages telephony)
+  #:use-module (gnu packages terminals)
+  #:use-module (gnu packages tex)
+  #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages tcl)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages lesstif)
+  #:use-module (gnu packages llvm)
+  #:use-module (gnu packages image)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages libevent)
+  #:use-module (gnu packages lisp-xyz)
+  #:use-module (gnu packages lua)
+  #:use-module (gnu packages music)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages w3m)
+  #:use-module (gnu packages web)
+  #:use-module (gnu packages wget)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages node)
+  #:use-module (gnu packages xml)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages acl)
+  #:use-module (gnu packages mail)
+  #:use-module (gnu packages messaging)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages pdf)
+  #:use-module (gnu packages racket)
+  #:use-module (gnu packages ruby)
+  #:use-module (gnu packages rust-apps)
+  #:use-module (gnu packages scheme)
+  #:use-module (gnu packages speech)
+  #:use-module (gnu packages xiph)
+  #:use-module (gnu packages mp3)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages fribidi)
+  #:use-module (gnu packages gd)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages password-utils)
+  #:use-module (gnu packages pulseaudio)
+  #:use-module (gnu packages sphinx)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages sqlite)
+  #:use-module (gnu packages ghostscript)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages haskell-xyz)
+  #:use-module (gnu packages wordnet)
+  #:use-module (gnu packages photo)
+  #:use-module (gnu packages uml)
+  #:use-module (gnu packages finance)
+  #:use-module (guix utils)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match))
+
+(define-public emacs-cursor-flash
+  (let ((commit "db74aa7b13422313fbbc64f674966cc10cd693a6")
+        (revision "1"))
+    (package
+      (name "emacs-cursor-flash")
+      (version (git-version "0.0.0" revision commit))
+      (source
+       (origin
+         (method git-fetch)
+         (uri (git-reference
+               (url "https://github.com/Boruch-Baum/emacs-cursor-flash")
+               (commit commit)))
+         (file-name (git-file-name name version))
+         (sha256
+          (base32
+           "13lpblp3gig5h4jb9pfqhgj0hifpj3aqh69qnki2y49rdrz5fkyd"))))
+      (build-system emacs-build-system)
+      (home-page "https://github.com/Boruch-Baum/emacs-cursor-flash")
+      (synopsis "Flashes a square of positions around POINT for a customizable interval")
+      (description "@code{emacs-cursor-flash} provides
+@code{cursor-flash} temporarily highlights the area immediately
+surrounding POINT when navigating amongst buffers and windows. It is
+intended to assist in visually identifying the current POINT and the
+current window when many windows are on the same frame or when windows
+are very large.")
+      (license license:gpl3+))))
---
+++ emacs-vline.scm
@@ -0,0 +1,137 @@
+;;; GNU Guix --- Functional package management for GNU
+;;; Copyright © 2021 Eric Brown <ecbrown@ericcbrown.com>
+;;;
+;;; This file is part of GNU Guix.
+;;;
+;;; GNU Guix is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or (at
+;;; your option) any later version.
+;;;
+;;; GNU Guix is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Guix.  If not, see <http://www.gnu.org/licenses/>.
+
+(define-module (emacs-vline)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (guix cvs-download)
+  #:use-module (guix download)
+  #:use-module (guix bzr-download)
+  #:use-module (guix git-download)
+  #:use-module (guix hg-download)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system cmake)
+  #:use-module (guix build-system copy)
+  #:use-module (guix build-system emacs)
+  #:use-module (guix build-system glib-or-gtk)
+  #:use-module (guix build-system perl)
+  #:use-module (guix build-system trivial)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages admin)
+  #:use-module (gnu packages aspell)
+  #:use-module (gnu packages audio)
+  #:use-module (gnu packages bash)
+  #:use-module (gnu packages cmake)
+  #:use-module (gnu packages code)
+  #:use-module (gnu packages cpp)
+  #:use-module (gnu packages curl)
+  #:use-module (gnu packages databases)
+  #:use-module (gnu packages dictionaries)
+  #:use-module (gnu packages djvu)
+  #:use-module (gnu packages ebook)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages golang)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages gtk)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages haskell-apps)
+  #:use-module (gnu packages ibus)
+  #:use-module (gnu packages java)
+  #:use-module (gnu packages ncurses)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-xyz)
+  #:use-module (gnu packages telephony)
+  #:use-module (gnu packages terminals)
+  #:use-module (gnu packages tex)
+  #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages tcl)
+  #:use-module (gnu packages tls)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages lesstif)
+  #:use-module (gnu packages llvm)
+  #:use-module (gnu packages image)
+  #:use-module (gnu packages linux)
+  #:use-module (gnu packages libevent)
+  #:use-module (gnu packages lisp-xyz)
+  #:use-module (gnu packages lua)
+  #:use-module (gnu packages music)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages imagemagick)
+  #:use-module (gnu packages w3m)
+  #:use-module (gnu packages web)
+  #:use-module (gnu packages wget)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages compression)
+  #:use-module (gnu packages node)
+  #:use-module (gnu packages xml)
+  #:use-module (gnu packages glib)
+  #:use-module (gnu packages acl)
+  #:use-module (gnu packages mail)
+  #:use-module (gnu packages messaging)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages perl)
+  #:use-module (gnu packages pdf)
+  #:use-module (gnu packages racket)
+  #:use-module (gnu packages ruby)
+  #:use-module (gnu packages rust-apps)
+  #:use-module (gnu packages scheme)
+  #:use-module (gnu packages speech)
+  #:use-module (gnu packages xiph)
+  #:use-module (gnu packages mp3)
+  #:use-module (gnu packages gettext)
+  #:use-module (gnu packages fribidi)
+  #:use-module (gnu packages gd)
+  #:use-module (gnu packages fontutils)
+  #:use-module (gnu packages password-utils)
+  #:use-module (gnu packages pulseaudio)
+  #:use-module (gnu packages sphinx)
+  #:use-module (gnu packages xdisorg)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages sqlite)
+  #:use-module (gnu packages ghostscript)
+  #:use-module (gnu packages gnupg)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages haskell-xyz)
+  #:use-module (gnu packages wordnet)
+  #:use-module (gnu packages photo)
+  #:use-module (gnu packages uml)
+  #:use-module (gnu packages finance)
+  #:use-module (guix utils)
+  #:use-module (srfi srfi-1)
+  #:use-module (ice-9 match))
+
+(define-public emacs-vline
+  (package
+    (name "emacs-vline")
+    (version "20120108")
+    (source
+     (origin
+       (method url-fetch)
+       (uri "https://www.emacswiki.org/emacs/download/vline.el")
+       (file-name "vline.el")
+       (sha256
+        (base32
+         "1ys6928fgk8mswa4gv10cxggir8acck27g78cw1z3pdz5gakbgnj"))))
+    (build-system emacs-build-system)
+    (home-page "https://www.emacswiki.org/emacs/vline.el")
+    (synopsis "Show vertical line (column highlighting) mode")
+    (description "@code{emacs-vline} provides @code{vline}, a
+mode for displaying a vertical line in a buffer.")
+    (license license:gpl2+)))
