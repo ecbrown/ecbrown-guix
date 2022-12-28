f0d94950b2a806b28458ecdb6ef79d98a09ba816
Eric Brown <ecbrown@ericcbrown.com>
2022-12-28 17:54:00 -0600

    Add python and python-build

--- python-gau2grid.scm
+++ python-gau2grid.scm
@@ -26,6 +26,8 @@
   #:use-module (gnu packages pkg-config)
   #:use-module (gnu packages python)
   #:use-module (gnu packages python-science)
+  #:use-module (gnu packages python)
+  #:use-module (gnu packages python-build)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages version-control)
   #:use-module (guix packages)
